eda48dd344d540f98a955a1834e0b79560db4253
Javier Albarracin <jalbarracn@gmail.com>
2017-06-15 09:46:03 -0300

    Add files via upload

---
+++ grafo.py
@@ -0,0 +1,109 @@
+#implementacion de un grafo
+
+# Definicion del TDA Grafo
+class Grafo:
+	def __init__(self, dir):
+			self.dic = {}
+			self.dir = dir
+			self.cant = 0
+
+# Devuelve True al quitar un vertice o False si el
+# vertice no pertenece al grafo.
+# Post: el grafo tiene un vertice menos
+	def	quitar_vertice(self, v):
+			if not self.existe_vertice(v):
+				return False
+
+			del self.dic[v]
+
+			for i in self.dic:
+				if v in self.dic[i]:
+					self.dic[i].remove(v)
+			self.cant -= 1
+			return True
+
+# Devuelve True al agregar un vertice al grafo,
+# False si el vertice ya pertenece al grafo
+	def	agregar_vertice(self, v):
+			if self.existe_vertice(v):
+				return False
+
+			self.dic[v] = []
+			self.cant += 1
+			return True
+
+# Devuelve True si un vertice pertenece al grafo,
+# False en caso contrario
+	def	existe_vertice(self, v):
+			return v in self.dic
+
+# Devuelve la cantidad de elementos en el grafo
+	def	cantidad(self):
+			return self.cant
+
+# devuelve la lista de vertices adyacentes al vertice
+# pasado por parametro o None si el vertice no existe
+	def	adyacentes(self, v):
+			if not self.existe_vertice(v):
+				return None
+
+			return self.dic[v]
+
+# devuelve True si dos vertices son adyacentes entre si
+# False en caso contrario
+#en un grafo dirigido, son_adyacentes(v1, v2) es True, pero son_adyacentes(v2, v1) es False
+#si v2 es adyacente de v1.
+#en un grafo no dirigido, tanto son_adyacentes(v1, v2) como son_adyacentes(v2, v1) son True
+#si v2 es adyacente de v1
+	def	son_adyacentes(self, v1, v2):
+			if not self.existe_vertice(v1) and not self.existe_vertice(v2):
+				return false
+
+			uno_con_dos = v2 in self.adyacentes(v1)
+
+			if self.dir:
+				return uno_con_dos
+
+			dos_con_uno = v1 in self.adyacentes(v2)
+
+			return uno_con_dos and dos_con_uno
+
+# Devuelve True al agregar una arista entre dos vertices
+# Si los vertices son adyacentes o alguno de ellos
+# no existe, devuelve False
+	def	agregar_arista(self, v1, v2):
+			if not self.existe_vertice(v1) or not self.existe_vertice(v2):
+				return False
+
+			if self.son_adyacentes(v1,v2):
+				return False
+
+			self.dic[v1].append(v2)
+
+			if not self.dir:
+				self.dic[v2].append(v1)
+
+			return True
+
+# Devuelve True al quitar una arista entre dos vertices.
+# Si los vertices no son adyacentes o alguno de ellos no existe,
+# devuelve False
+	def	quitar_arista(self, v1, v2):
+			if not self.existe_vertice(v1) or not self.existe_vertice(v2):
+				return False
+
+			if not self.son_adyacentes(v1,v2):
+				return False
+
+			self.dic[v1].remove(v2)
+
+			if not self.dir:
+				self.dic[v2].remove(v1)
+
+			return True
+
+#Devuelve una lista con los Vertices del grafo.
+	def vertices(self):
+		return list(self.dic.keys())
+
+#Proximamente: iterador
---
+++ pruebas_grafo.py
@@ -0,0 +1,66 @@
+#comment
+from grafo import Grafo
+
+#	class	Grafo:
+#		def __init__(self, dir):		dir = True para un grafo dirigido, False para uno no dirigido
+#			self.dic = {}				diccionario vacio
+#			self.dir = dir				direccion
+#			self.cant					cantidad
+
+#	quitar vertice
+#	agregar vertice
+#	quitar arista
+#	agregar arista
+#	son adyacentes
+#	adyacentes
+#	cantidad
+#	existe vertice
+#
+
+#Pruebas sobre un grafo no dirigido
+print("Crear grafo no dirigido")
+g = Grafo(False)
+
+print("Cantidad de elementos en grafo vacio es 0... OK")			if g.cantidad() == 0				else print("Cantidad de elementos en grafo vacio es 0... Error")
+print("Agregar vertice '1' es True... OK")							if g.agregar_vertice('1')			else print("Agregar vertice '1' es True... Error")
+print("Agregar de nuevo vertice '1' es False... OK")				if not g.agregar_vertice('1')		else print("Agregar de nuevo vertice '1' es False... Error")
+print("Cantidad de elementos es 1... OK")							if g.cantidad() == 1				else print("Cantidad de elementos es 1... Error")
+print("Agregar vertice '2' es True... OK")							if g.agregar_vertice('2')			else print("Agregar vertice '2' es True... Error")
+print("Agregar de nuevo vertice '2' es False... OK")				if not g.agregar_vertice('2')		else print("Agregar de nuevo vertice '2' es False... Error")
+print("1 y 2 son adyacentes es False... OK")						if not g.son_adyacentes('1', '2')	else print("1 y 2 son adyacentes es False... Error")
+print("Quitar arista entre 1 y 2 es False... OK")					if not g.quitar_arista('1', '2')	else print("Quitar arista entre 1 y 2 es False... Error")
+print("Cantidad de elementos es 2... OK")							if g.cantidad() == 2				else print("Cantidad de elementos es 2... Error")
+print("Agregar arista entre 1 y 2 es True... OK")					if g.agregar_arista('1', '2')		else print("Agregar arista entre 1 y 2 es True... Error")
+print("Agregar arista entre 1 y 2 de nuevo es False... OK")			if not g.agregar_arista('1', '2')	else print("Agregar arista entre 1 y 2 de nuevo es False... Error")
+print("1 y 2 son adyacentes es True... OK")							if g.son_adyacentes('1', '2')		else print("1 y 2 son adyacentes es True... Error")
+print("Existe vertice 1 es True... OK")								if g.existe_vertice('1')			else print("Existe vertice 1 es True... Error")
+print("Existe vertice 2 es True... OK")								if g.existe_vertice('2')			else print("Existe vertice 2 es True... Error")
+
+vertices = g.vertices()
+
+print("Elemento 0 en lista de vertices es 1... OK")					if vertices[0] == '1'				else print("Elemento 0 en lista de vertices es 1... Error")
+print("Elemento 1 en lista de vertices es 2... OK")					if vertices[1] == '2'				else print("Elemento 1 en lista de vertices es 2... Error")
+
+ady1 = g.adyacentes('1')
+ady2 = g.adyacentes('2')
+
+print("Elemento en lista de adyacencia de 1 es 2... OK")			if ady1[0] == '2'					else print("Elemento en lista de adyacencia de 1 es 2... Error")
+print("Elemento en lista de adyacencia de 2 es 1... OK")			if ady2[0] == '1'					else print("Elemento en lista de adyacencia de 2 es 1... Error")
+print("Quitar arista entre 1 y 2 es True... OK")					if g.quitar_arista('1', '2')		else print("Quitar arista entre 1 y 2 es True... Error")
+print("Quitar arista entre 1 y 2 es False... OK")					if not g.quitar_arista('1', '2')	else print("Quitar arista entre 1 y 2 es False... Error")
+print("1 y 2 son adyacentes es False... OK")						if not g.son_adyacentes('1', '2')	else print("1 y 2 son adyacentes es False... Error")
+print("Quitar vertice 1 es True... OK")								if g.quitar_vertice('1')			else print("Quitar vertice 1 es True... Error")
+print("Quitar vertice 1 de nuevo es False... OK")					if not g.quitar_vertice('1')		else print("Quitar vertice 1 de nuevo es False... Error")
+print("Quitar vertice 2 es True... OK")								if g.quitar_vertice('2')			else print("Quitar vertice 2 es True... Error")
+print("Quitar vertice 2 de nuevo es False... OK")					if not g.quitar_vertice('2')		else print("Quitar vertice 2 de nuevo es False... Error")
+
+print("Crear grafo dirigido")
+h = Grafo(True)
+
+print("Agregar de vertice '1' es True... OK")						if h.agregar_vertice('1')			else print("Agregar vertice '1' es True... Error")
+print("Cantidad de elementos es 1... OK")							if h.cantidad() == 1				else print("Cantidad de elementos es 1... Error")
+print("Agregar vertice '2' es True... OK")							if h.agregar_vertice('2')			else print("Agregar vertice '2' es True... Error")
+print("Agregar de nuevo vertice '2' es False... OK")				if not h.agregar_vertice('2')		else print("Agregar de nuevo vertice '2' es False... Error")
+print("Agregar arista entre 1 y 2 es True... OK")					if h.agregar_arista('1', '2')		else print("Agregar arista entre 1 y 2 es True... Error")
+print("1 y 2 son adyacentes es True... OK")							if h.son_adyacentes('1', '2')		else print("1 y 2 son adyacentes es True... Error")
+print("2 y 1 son adyacentes es False... OK")						if not h.son_adyacentes('2', '1')	else print("2 y 1 son adyacentes es False... Error")

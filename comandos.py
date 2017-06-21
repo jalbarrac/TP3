175e7821f25f91ac9a219bc77f26ccb693fcb277
Javier Albarracin <jalbarracn@gmail.com>
2017-06-21 00:24:16 -0300

    Add files via upload

--- comandos.py
+++ comandos.py
@@ -1,5 +1,53 @@
 import sys
+import comunidades
+import itertools
+import random
 from grafo import Grafo
+
+MAX_ITERACIONES = 100
+
+def max_freq(grafo, label, v):
+	freq = {}
+	for u in grafo.adyacentes(v):
+		if label[u] not in freq:
+			freq[label[u]] = 0
+		else:
+			freq[label[u]] += 1
+
+	max = 0
+	maxlabel = 0
+	for clave, valor in freq.items():
+		if valor > max:
+			max = valor
+			maxlabel = clave
+	
+	return maxlabel
+
+
+
+def label_propagation(grafo):
+	label = {}
+	vertices = grafo.vertices()
+
+	for i, v in enumerate(sorted(vertices)):
+		label[v] = i + 1
+
+	#for v in sorted(label.keys()):
+	#	print(v, label[v])
+
+	
+	for i in (0, MAX_ITERACIONES):
+
+		random.shuffle(vertices)
+		for v in vertices:
+			label[v] = max_freq(grafo, label, v)
+
+	for l in label:
+		print(l)
+
+
+
+#estadisticas--------------------------------------------------------
 
 def mostrar_estadisticas(grafo):
 
@@ -11,15 +59,21 @@ def mostrar_estadisticas(grafo):
 	print("Cantidad de vertices: ", vertices)
 	print("Cantidad de aristas: ", aristas)
 
-	promedio = aristas / vertices
+	promedio = 2 * aristas / vertices
 
 	print("Promedio grado de entrada de cada vertice: ", promedio)
 	print("Promedio grado de salida de cada vertice: ", promedio)
 
 	densidad = (2 * aristas)/(vertices * (vertices - 1))
-
 	print("Densidad del grafo: ", densidad)
-	
+
+
+
+#Programa principal
+
+if (len(sys.argv) < 2):
+	print("Programa: ruta de archivo no especificada")
+	raise SystemExit
 
 archivo = sys.argv[1]
 g = Grafo(False)
@@ -27,23 +81,42 @@ g = Grafo(False)
 print("Ingrese 'Salir' para salir.")
 print("Cargando grafo... ")
 
-with open(archivo, 'r') as datos:
-	for i in (0, 4):
-		next(datos)
+with open(archivo, 'r') as arch:
+	datos = itertools.islice(arch, 4, None)
 	for linea in datos:
 		vtcs = linea.split()
 
-		g.agregar_vertice(vtcs[0])
-		g.agregar_vertice(vtcs[1])
-		g.agregar_arista(vtcs[0], vtcs[1])
+		g.agregar_vertice(int(vtcs[0]))
+		g.agregar_vertice(int(vtcs[1]))
+		g.agregar_arista(int(vtcs[0]), int(vtcs[1]))
 
-	datos.close()
+	arch.close()
 
 print("Grafo cargado en memoria.")
 
-comando = input().lower()
-while(comando != "salir"):
-	if(comando == "estadisticas"):
-		mostrar_estadisticas(g);
 
-	comando = input()
+while(True):
+	comando = input().lower()
+	args = comando.split()
+
+	if(len(args) == 1):
+		if(args[0] == "salir"):
+			break
+		elif(args[0] == "estadisticas"):
+			mostrar_estadisticas(g)
+		elif(args[0] == "comunidades"):
+			#comunidades(args, g)
+			label_propagation(g)
+		elif(args[0] == "distancias"):
+			distancias(args, g)
+		elif(args[0] == "centralidad"):
+			centralidad(args, g)
+		elif(args[0] == "camino"):
+			camino(args, g)
+		elif(args[0] == "recomendar"):
+			recomendar(args, g)
+		elif(args[0] == "similares"):
+			similares(args, g)
+		else:
+			print("Programa: comando \""  + args[0] + "\" no reconocido.")
+
